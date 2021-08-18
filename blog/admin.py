5e0efd5d03a237caa9b7919e311bd99ae9945254
Vagif Molla <vagif69@gmail.com>
2021-08-18 14:11:29 +0300

    Add new model Category, register in admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category')
+    list_display_links = ('id', 'category')
+    search_fields = ('id', 'category')
+    list_editable = ('category',)
+
+
+admin.site.register(Category, CategoryAdmin)
