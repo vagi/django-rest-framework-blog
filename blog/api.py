064453862b2409d727d96833a7b0631427f382bc
Vagif Molla <vagif69@gmail.com>
2021-08-19 17:23:38 +0300

    Add querysets for each model, add router with urls

--- blog/api.py
+++ blog/api.py
@@ -1,12 +1,36 @@
-from .models import Category
+from .models import Category, Author, Post, Comment
 from rest_framework import viewsets, permissions
-from .serializers import CategorySerializer
+from .serializers import CategorySerializer, AuthorSerializer, PostSerializer, CommentSerializer
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     permission_classes = [
-        permissions.AllowAny
+        permissions.AllowAny    # change to IsAuthenticated
     ]
     serializer_class = CategorySerializer
+
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = AuthorSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = PostSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = CommentSerializer
 
--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from rest_framework import routers
-from .api import CategoryViewSet
+from .api import CategoryViewSet, AuthorViewSet, PostViewSet, CommentViewSet
 
 
 router = routers.DefaultRouter()
 router.register('api/categories', CategoryViewSet, 'categories')
+router.register('api/authors', AuthorViewSet, 'authors')
+router.register('api/posts', PostViewSet, 'posts')
+router.register('api/comments', CommentViewSet, 'comments')
 
 urlpatterns = router.urls
