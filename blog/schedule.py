1c8067cd3456279a40130079703ffc1b1ab8e7e1
Vagif Molla <vagif69@gmail.com>
2021-09-14 16:33:26 +0300

    Add import of two modules from __future__

--- blog/schedule.py
+++ blog/schedule.py
@@ -5,7 +5,7 @@ from celery.schedules import crontab
 CELERYBEAT_SCHEDULE = {
     'change_author_is_notified_to_true': {
         'task': 'change_author_is_notified_to_true',
-        'schedule': crontab(minute=2),
+        'schedule': crontab(),
     },
 }
 
--- blog/tasks.py
+++ blog/tasks.py
@@ -12,9 +12,9 @@ def change_author_is_notified_to_true():
 
     for author_id in author_ids:
         q = Author.objects.get(pk=author_id)
-        #print("Query before change:", q.is_notified)   # debugging
-        #print("Author_Id is:", author_id)    # debugging
+        print("Query before change:", q.is_notified)   # debugging
+        print("Author_Id is:", author_id)    # debugging
         q.is_notified = True
         q.save()
-        #print("Query after change:", q.is_notified)   # debugging
+        print("Query after change:", q.is_notified)   # debugging
 
